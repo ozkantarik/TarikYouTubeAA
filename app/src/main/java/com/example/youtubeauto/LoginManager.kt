cbc8070b185b55118537165d1f2f3939f91288d5
Tarik Ozkan <ozkantarik@users.noreply.github.com>
2025-05-28 03:09:56 +0300

    Add Android Auto project skeleton

---
+++ app/src/main/java/com/example/youtubeauto/LoginManager.kt
@@ -0,0 +1,13 @@
+package com.example.youtubeauto
+
+object LoginManager {
+    /**
+     * This is a placeholder for a login system. In a real application
+     * this would retrieve the user's account information from the
+     * connected phone using Google APIs.
+     */
+    fun isLoggedIn(): Boolean {
+        // Always return true for this sample
+        return true
+    }
+}
---
+++ app/src/main/java/com/example/youtubeauto/PlaylistsFragment.kt
@@ -0,0 +1,5 @@
+package com.example.youtubeauto
+
+import androidx.fragment.app.Fragment
+
+class PlaylistsFragment : Fragment(R.layout.fragment_playlists)
